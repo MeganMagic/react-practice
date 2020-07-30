4e3b2243956b6b44433ba78efc2ebfa6aff8f470
Megan <at.fence.3@gmail.com>
2020-07-30 12:24:03 +0900

    update all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Counter from './03/Counter';
+import NewCounter from './03/NewCounter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {count : 10};
+    this.resetCount = this.resetCount.bind(this);
+  }
+
+  resetCount() {
+    this.setState(({count}) => ({count:count + 10}));
+  }
+
+  render(){
+    return (
+      <div>
+        <div><Counter count={this.state.count} /></div>
+        <div><NewCounter count={this.state.count} /></div>
+        <button onClick={this.resetCount} >{this.state.count+10}으로 초기화</button>
+      </div>
+    )
+  }
 }
 
 export default App;
